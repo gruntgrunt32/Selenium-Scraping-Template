d5a5ccc8e48853c0efbd31289377936e01a030d1
Austin Reed <areed9953@gmail.com>
2024-03-23 14:34:52 -0300

    Initial commit

---
+++ selenium_template.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import pickle
+import time
+import random
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def save_cookies(driver):
+    pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
+
+def load_cookies(driver):
+    try:
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+    except:
+        pass
+
+def browser_init():
+    chrome_options = Options()
+    # add proxy to chrome options 108.59.14.203:13010
+    # chrome_options.add_argument("--proxy-server=108.59.14.203:13010")
+    # Mimic a real user's browser by setting user agent and user data directory.
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36")
+    chrome_options.add_argument("--user-data-dir=C:\\Users\\omnil\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+    # Add more options to make automated browser more like a regular user's browser.
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver.get('https://www.google.com/')
+    load_cookies(driver)
+    return driver
+
+
+def random_sleep():
+    time.sleep(random.randint(1, 4))
+
+def wait_for_element(driver, xpath, timeout=30):
+    start_time = time.time()
+    while True:
+        try:
+            element = driver.find_element(By.XPATH, xpath)
+            return element
+        except:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            if elapsed_time > timeout:
+                print(f"Timeout reached after {timeout} seconds")
+                break
+            pass
+
+
+if __name__ == "__main__":
+    driver = browser_init()
+    
+
+
+
